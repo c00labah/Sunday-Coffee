85c1f94e9a5fde8530af4abfb50427f95ed1f595
kallej8 <joshkallenbach@icloud.com>
2026-01-18 21:00:10 +1100

    Initial Commit

---
+++ Sunday Coffee/Sunday_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunday_CoffeeApp.swift
+//  Sunday Coffee
+//
+//  Created by Paul Kallenbach on 18/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct Sunday_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sunday CoffeeTests/Sunday_CoffeeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Sunday_CoffeeTests.swift
+//  Sunday CoffeeTests
+//
+//  Created by Paul Kallenbach on 18/1/2026.
+//
+
+import Testing
+
+struct Sunday_CoffeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
